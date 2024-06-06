ef589f3fe18528eeef9f6cd62f82603bfd655345
Manuel Otero <manuelotero@Manuels-MacBook-Pro.local>
2024-06-06 11:36:18 +0200

    se remueve el tercer parametro de opent at por error en docker

---
+++ test/integrity/test_xpn_syscall/cat.sh
@@ -0,0 +1,5 @@
+INTERCEPT_LOG=logs/intercept.log- \
+LD_LIBRARY_PATH=. \
+LD_PRELOAD=/home/lab/src/xpn/src/xpn_syscall_intercept/xpn_syscall_intercept.so \
+XPN_CONF=./xpn.conf \
+cat /tmp/expand/P1/demo.txt
